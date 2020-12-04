efd905c7db1ffb5a3b8e7c9078dade9c608993f4
Nguyen Tong Ngoc Ha <hapro574@gmail.com>
2020-12-04 12:30:16 +0700

    thiết kế khung module

---
+++ module/album/admin.menu.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author VINADES.,JSC <contact@vinades.vn>
+ * @Copyright (C) 2020 VINADES.,JSC. All rights reserved
+ * @License: Not free read more http://nukeviet.vn/vi/store/modules/nvtools/
+ * @Createdate Tue, 10 Nov 2020 06:56:08 GMT
+ */
+
+if (!defined('NV_ADMIN')) {
+    die('Stop!!!');
+}
+
+$submenu['create_album'] = $lang_module['create_album'];
+$submenu['create_image'] = $lang_module['create_image'];
+$submenu['image'] = $lang_module['image'];
+
+$submenu['config'] = $lang_module['config'];
